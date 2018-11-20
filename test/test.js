7fc0f900dfad7463f89cd4a87f774125bf15fe87
foryuki <foryuki@outlook.com>
2018-11-20 14:23:53 +0800

    test: add snapshot testing

--- test/test.js
+++ test/test.js
@@ -5,7 +5,21 @@ import sao from 'sao'
 const generator = path.join(__dirname, '..')
 
 test('defaults', async t => {
-  const stream = await sao.mock({ generator })
+  const mockPromptAnswers = {
+    name: 'cloud',
+    username: 'username',
+    email: 'placholder@mymail.com'
+  }
+
+  const stream = await sao.mock({generator}, mockPromptAnswers)
 
   t.snapshot(stream.fileList, 'Generated files')
+
+  const pkg = stream.readFile('package.json')
+  // t.is(pkg.name, 'cloud')
+  // t.is(pkg.username, 'username')
+  // t.is(pkg.email, 'placholder@mymail.com')
+
+  t.snapshot(stream.readFile('readme.md'), 'readme.md')
+  t.snapshot(stream.readFile('.editorconfig'), '.editorconfig')
 })
